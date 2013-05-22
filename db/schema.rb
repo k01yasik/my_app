040e5cd30b34cee4eca3ccd36f02b0e8ea2f0f9a
Nickolay A. Smirnov <bzdykin@mail.ru>
2013-05-22 09:43:00 +0400

    k

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518093305) do
+ActiveRecord::Schema.define(:version => 20130520075952) do
 
-  create_table 'cars', :force => true do |t|
-    t.string 'brand'
-    t.string 'model'
-    t.datetime 'created_at', :null => false
-    t.datetime 'updated_at', :null => false
+  create_table "cars", :force => true do |t|
+    t.string   "brand"
+    t.string   "model"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
+
+  create_table "person", :force => true do |t|
+    t.string   "name"
+    t.string   "last_name"
+    t.integer  "car_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "person", ["car_id"], :name => "index_person_on_car_id"
 
 end
